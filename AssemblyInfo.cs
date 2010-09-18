fae39d3142ad2380db9642d772dd403d719a2ef1
Jonas Gorauskas <jgorauskas@gmail.com>
2010-09-18 10:22:11 -0700

    Initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XmlTidy")]
+[assembly: AssemblyDescription("Tidy up XML files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonas Gorauskas")]
+[assembly: AssemblyProduct("XmlTidy")]
+[assembly: AssemblyCopyright("© 2010 Jonas Gorauskas")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bb6ced5-f522-486c-a1af-1de63a85ba1b")]
+
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.*")]
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+
+using Mono.Options;
+
+namespace XmlTidy 
+{
+  class Program 
+  {
+    static void Main(string[] args) 
+    {
+      ParseCommandLine(args);
+      try 
+      {
+        XmlTidy(args[0]);
+      } 
+      catch (FileNotFoundException ex) 
+      {
+        Console.WriteLine("{0}: {1}", ex.Message, ex.FileName);
+      }
+    }
+
+    static void XmlTidy(string filename) 
+    {
+      if (File.Exists(filename)) 
+      {
+        var doc = XDocument.Load(filename);
+        doc.Save(filename, SaveOptions.None);
+      } 
+      else 
+      {
+        throw new FileNotFoundException("File not found", filename);
+      }
+    }
+
+    static void ParseCommandLine(string[] args)
+    {
+      
+    }
+
+    static void ShowVersion()
+    {
+      
+    }
+
+    static void ShowUsage()
+    {
+      
+    }
+
+    
+  }
+}
