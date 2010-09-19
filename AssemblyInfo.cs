02168c5cc3aa74ac322e3b9744c7ecc6877003e2
Jonas Gorauskas <jgorauskas@gmail.com>
2010-09-18 19:24:45 -0700

    Build file for mono on linux; ignore file updates

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,10 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© 2010 Jonas Gorauskas")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("9bb6ced5-f522-486c-a1af-1de63a85ba1b")]
-
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
--- build.sh
+++ build.sh
@@ -1,1 +1,22 @@
-#
+#!/bin/sh
+
+# Build XMLTidy on Linux using Mono
+# if the mcs/gmcs binary is not recognized, you may need to install it
+
+if [ -d "./bin" ]; then
+    echo "Build output directory already exists."
+else
+    echo "Create build output directory."
+    mkdir ./bin
+    cp ./mono/Mono.Options.dll ./bin/Mono.Options.dll
+    cp ./mono/Mono.Options.dll.mdb ./bin/Mono.Options.dll.mdb
+fi
+
+echo "Start program build"
+
+gmcs -pkg:dotnet /r:System.Xml.Linq.dll /r:./bin/Mono.Options.dll /nologo /out:./bin/xmltidy.exe AssemblyInfo.cs Program.cs
+
+echo "DONE! Run mono ./bin/xmltidy.exe --help for usage"
+echo "."
+
+
